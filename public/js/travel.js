d93af97a9f7f453564883aad7fb9583e903a94be
麋鹿人 <whoistoto@gmail.com>
2014-12-24 14:42:02 +0800

    報名頁語法整理

---
+++ public/js/travel.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+   $("#accordion").accordion({
+      heightStyle: "content",
+      collapsible: true,
+      active: false,
+      beforeActivate: function(event, ui) {
+       // The accordion believes a panel is being opened
+      if (ui.newHeader[0]) {
+          var currHeader  = ui.newHeader;
+          var currContent = currHeader.next('.ui-accordion-content');
+       // The accordion believes a panel is being closed
+      } else {
+          var currHeader  = ui.oldHeader;
+          var currContent = currHeader.next('.ui-accordion-content');
+      }
+       // Since we've changed the default behavior, this detects the actual status
+      var isPanelSelected = currHeader.attr('aria-selected') == 'true';
+
+       // Toggle the panel's header
+      currHeader.toggleClass('ui-corner-all',isPanelSelected).toggleClass('accordion-header-active ui-state-active ui-corner-top',!isPanelSelected).attr('aria-selected',((!isPanelSelected).toString()));
+      // Toggle the panel's icon
+      currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e',isPanelSelected).toggleClass('ui-icon-triangle-1-s',!isPanelSelected);
+      // Toggle the panel's content
+      currContent.toggleClass('accordion-content-active',!isPanelSelected)    
+      if (isPanelSelected) { currContent.slideUp(); }  else { currContent.slideDown(); }
+
+      return false; // Cancels the default action
+    }
+  });
+  $(function(){ 
+    var _showTab = 0;
+    var $defaultLi = $('#tab li').eq(_showTab).addClass('active');
+    $($defaultLi.find('a').attr('href')).siblings().hide();
+    $('#tab li').click(function() {   
+      var $this = $(this),
+        _clickTab = $this.find('a').attr('href');   
+      $this.addClass('active').siblings('.active').removeClass('active');   
+      $(_clickTab).stop(false, true).fadeIn().siblings().hide(); 
+      return false;
+      }).find('a').focus(function(){
+        this.blur();
+      });
+    });    
+  }); 
