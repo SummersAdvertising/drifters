a5a8daa89b7630c803e3b7e091f49e575076b7cd
oxcart <oxcart.us@gmail.com>
2016-01-11 15:28:18 +0800

    修正行程名稱

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 #encoding: utf-8
 
-Admin.create :email => 'adam@summers.com.tw', :password => '12345678'
+# Admin.create :email => 'adam@summers.com.tw', :password => '12345678'
+
+
+t = Tour.find(8)
+t.name = "納米比亞秘境野營17天"
+t.en_name = 'Namibia_17_day'
+t.save
